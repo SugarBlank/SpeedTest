2f81960345bbf6caa9835647bb3e13b561f5d9c8
scouri <eneblank19@protonmail.com>
2021-07-03 14:40:36 -0400

    Create PingTest.java

---
+++ src/java/com/example/speedtest/tests/PingTest.java
@@ -0,0 +1,71 @@
+package com.example.speedtest.tests;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.nio.Buffer;
+import java.util.HashMap;
+
+public class PingTest extends Thread{
+    HashMap<String, Object> result = new HashMap<String, Object>();
+    String server;
+    int count;
+    double instantRoundTripTime;
+    double averageRoundTripTime;
+    boolean finished = false;
+    boolean started = false;
+    Process process;
+
+    public PingTest(String serverIpAddress, int AttemptCount)
+    {
+        this.server = serverIpAddress;
+        this.count = AttemptCount;
+    }
+    public double getAverageRoundTripTime()
+    {
+        return averageRoundTripTime;
+    }
+    public double getInstantRoundTripTime()
+    {
+        return instantRoundTripTime;
+    }
+    public boolean isFinished()
+    {
+        return finished;
+    }
+    @Override
+    public void run()
+    {
+        try
+        {
+            ProcessBuilder processBuilder = new ProcessBuilder("ping", "-c" + count, this.server);
+            processBuilder.redirectErrorStream(true);
+            Process process = processBuilder.start();
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+            String line;
+            while ((line = bufferedReader.readLine()) != null)
+            {
+                if (line.contains("icmp_seq"))
+                {
+                    instantRoundTripTime = Double.parseDouble(line.split(" ")[line.split(" ").length - 2].replace("time=", ""));
+                }
+                if (line.startsWith("rtt"))
+                {
+                    averageRoundTripTime = Double.parseDouble(line.split("/")[4]);
+                    break;
+                }
+                if (line.contains("Unreachable") || line.contains("Unknown") || line.contains("%100 packet loss"))
+                {
+                    return;
+                }
+            }
+            process.waitFor();
+            bufferedReader.close();
+
+        }
+        catch (Exception exception)
+        {
+            exception.printStackTrace();
+        }
+        finished = true;
+    }
+}
