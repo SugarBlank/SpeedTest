be15cbcf17130e5982b55b4a5ae1ce3f35c2080c
scouri <eneblank19@protonmail.com>
2021-07-19 18:31:54 -0400

    Delete src/java/com/example/speedtest/ui directory

--- src/java/com/example/speedtest/ui/Test.java
+++
@@ -1,9 +0,0 @@
-package com.example.speedtest.ui;
-import fr.bmartel.speedtest.SpeedTestReport;
-import fr.bmartel.speedtest.SpeedTestSocket;
-import fr.bmartel.speedtest.inter.ISpeedTestListener;
-import fr.bmartel.speedtest.model.SpeedTestError;
-
-public class Test {
-
-}
